f89fa2473b35a87219b80173cee31a287f7896e8
Josep Ll. Gelpí <gelpi@ub.edu>
2026-01-30 14:07:16 +0100

    Fixed NGL viewer

--- portfolio/js/ngl.js
+++ portfolio/js/ngl.js
@@ -1,4 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var stage = new NGL.Stage("viewport");
-    stage.loadFile("rcsb://1crn", {defaultRepresentation: true});
-  });
+    // Initialize NGL Stage
+    var stage = new NGL.Stage("viewport", {
+        backgroundColor: "white"
+    });
+    
+    // Load protein structure from RCSB PDB
+    stage.loadFile("https://mdb-login.bsc.es/api/pdb/1crn.pdb").then(function (component) {
+        // Add different representation styles
+        component.addRepresentation("cartoon", {
+            color: "secondary structure"
+        });
+        component.addRepresentation("ball+stick", {
+            sele: "hetero",
+            color: "element"
+        });
+        
+        // Auto-center and zoom to fit
+        component.autoView();
+    });
+    
+    // Handle window resizing
+    window.addEventListener("resize", function () {
+        stage.handleResize();
+    }, false);
+});
